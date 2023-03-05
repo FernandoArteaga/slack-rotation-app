353a7a17db479e3758d7adace6410a8a3c4682d0
Fernando Arteaga <fernando.arteaga@unity3d.com>
2023-03-04 19:48:11 -0500

    installed and configured slack/web-api

---
+++ functions/CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
+
+## [0.0.2] - 2023-03-04
+
+### Added
+
+- Cloud functions for handling [Events](https://api.slack.com/apis/connections/events-api)
+- Cloud functions for different events (basic functionalities):
+  - `app_mention`
+  - `app_home_opened`
+- [Block Kit](src/blocks) directory for storing all App related blocks
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,54 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+import { PubSub } from '@google-cloud/pubsub'
+import { WebClient } from '@slack/web-api';
+import requestValidator from './requestValidator';
+import appHome from './blocks/appHome';
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const REGION = 'us-east4'
+const bot = new WebClient(process.env.SLACK_TOKEN);
+const pubSubClient = new PubSub();
+
+exports.events = functions.region(REGION).https.onRequest(async (request, response) => {
+  requestValidator(request)
+
+  const body = request.body
+
+  functions.logger.info(body.type)
+  functions.logger.info(body.event)
+
+  const data = JSON.stringify(request.body);
+  const dataBuffer = Buffer.from(data);
+
+  await pubSubClient
+    .topic(body.event.type)
+    .publishMessage({data: dataBuffer});
+
+  response.sendStatus(200)
 });
+
+exports.eventAppMention = functions.region(REGION).pubsub
+  .topic('app_mention')
+  .onPublish(async (message) => {
+    const { event } = message.json;
+    const { channel, text } = event;
+
+    await bot.chat.postMessage({
+      channel: channel,
+      text: `You just said: "${ text }"`
+    })
+  });
+
+exports.eventAppHomeOpened = functions.region(REGION).pubsub
+  .topic('app_home_opened')
+  .onPublish(async (message) => {
+    const { event } = message.json;
+    const { user } = event;
+
+    await bot.views.publish({
+      user_id: user,
+      view: {
+        type: 'home',
+        blocks: appHome
+      }
+    })
+  });
